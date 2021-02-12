92cf045332b836364a84bd493f172d3f445af7fb
RamseyCRV <robert.crv@icloud.com>
2021-02-12 17:52:21 +0200

    Menu href for navigation

--- src/main/java/Libra/SpringBoot/FullStackProject/LibraWebsite/Backoffice/Controllers/EmployeeController.java
+++ src/main/java/Libra/SpringBoot/FullStackProject/LibraWebsite/Backoffice/Controllers/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
-    @GetMapping(value = "/backoffice_employee")
+    @GetMapping(value = "/backoffice_employees")
     public String getEmployee(){
         return "./Backoffice/employee";
     }
